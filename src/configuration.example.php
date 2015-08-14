4c3ccb33dc47b7defc473eb6fd630e479265a0a5
Václav Makeš <info@makes.cz>
2015-08-14 15:30:33 +0200

    Added function for register user
    Do not work valid used email.

---
+++ src/configuration.example.php
@@ -0,0 +1,6 @@
+<?php
+
+const DB_NAME = '';
+const DB_HOSTNAME = '';
+const DB_USER = '';
+const DB_PASSWORD = '';
--- src/index.php
+++ src/index.php
@@ -1,6 +1,7 @@
 <?php
 
 require 'vendor/autoload.php';
+require 'Model.php';
 
 $result = [
 	'surname' => 'John',
@@ -20,5 +21,25 @@ $app->get('/get-info/:id', function ($id) use ($app, $result) {
 	$response['Content-Type'] = 'application/json';
 	$response->status(200);
 	$response->body(json_encode($result));
+});
+$app->post('/add', function() use ($app) {
+	$response = $app->response();
+	$response['Content-Type'] = 'application/json';
+	$request = $app->request;
+	$email = $request->post('email');
+	$password = $request->post('password');
+	if ($email == null || $password == null) {
+		$response->status(400);
+		$response->body(json_encode(['error' => 'Data are not complete.']));
+	}
+	$pdo = connectDB();
+	if (existsEmailInUsers($pdo, $email)) {
+		$response->status(400);
+		$response->body(json_encode(['error' => 'Email is already registered.']));
+	}
+	registerWithPassword($pdo, $email, $password);
+	$response['Content-Type'] = 'application/json';
+	$response->status(201);
+	$response->body(json_encode(['success' => 'User was created.']));
 });
 $app->run();
