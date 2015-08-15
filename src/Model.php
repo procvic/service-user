2e310f18b0a6a8627bbd042e6c5358ce32bfa41b
= <pavel.mlejnek@hotmail.com>
2015-08-15 17:34:38 +0200

    Update coding style

--- src/Model.php
+++ src/Model.php
@@ -1,10 +1,11 @@
 <?php
 
 if (!file_exists('configuration.php')) {
-	$erorrConfigurationMessage = 'You have to create a file \'configuration.php\'. '
-		. 'You can inspire in \'configuration.example.php\'';
-	throw new Exception($erorrConfigurationMessage);
+    $erorrConfigurationMessage = 'You have to create a file \'configuration.php\'. '
+        . 'You can inspire in \'configuration.example.php\'';
+    throw new Exception($erorrConfigurationMessage);
 }
+
 require 'configuration.php';
 
 const ROLE_USER = 'user';
@@ -15,17 +16,19 @@ const MAIL_VERIFIED = 1;
 
 function connectDB()
 {
-	$dsn = 'mysql:dbname=' . DB_NAME . ';host=' . DB_HOSTNAME;
-	$user = DB_USER;
-	$password = DB_PASSWORD;
+    $dsn = 'mysql:dbname=' . DB_NAME . ';host=' . DB_HOSTNAME;
+    $user = DB_USER;
+    $password = DB_PASSWORD;
 
-	try {
-		$dbh = new PDO($dsn, $user, $password);
-	} catch (PDOException $e) {
-		throw new Exception('Connection failed: ' . $e->getMessage());
-	}
-	$dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-	return $dbh;
+    try {
+        $dbh = new PDO($dsn, $user, $password);
+    } catch (PDOException $e) {
+        throw new Exception('Connection failed: ' . $e->getMessage());
+    }
+
+    $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+    return $dbh;
 }
 
 
@@ -35,60 +38,67 @@ function connectDB()
  */
 function registerWithPassword(PDO $dbh, $email, $password)
 {
-	$userData = [
-		'email' => $email,
-		'password' => \Nette\Security\Passwords::hash($password),
-		'role' => ROLE_USER,
-	];
-	$hash = sha1($email . time());
-	$emailData = [
-		'hash' => $hash,
-		'verified' => MAIL_UNVERIFIED
-	];
+    $userData = [
+        'email' => $email,
+        'password' => \Nette\Security\Passwords::hash($password),
+        'role' => ROLE_USER,
+    ];
 
-	$sql = '
+    $hash = sha1($email . time());
+
+    $emailData = [
+        'hash' => $hash,
+        'verified' => MAIL_UNVERIFIED
+    ];
+
+    $sql = '
 		INSERT INTO users (email, password, role)
 		VALUES(:email, :password, :role)';
-	$sth = $dbh->prepare($sql);
-	$sth->execute($userData);
+    $sth = $dbh->prepare($sql);
+    $sth->execute($userData);
 
-	$sql = '
+    $sql = '
 		INSERT INTO users_verified_emails (hash, verified, user)
 		VALUES(:hash, :verified, LAST_INSERT_ID())';
-	$sth = $dbh->prepare($sql);
-	$sth->execute($emailData);
+    $sth = $dbh->prepare($sql);
+    $sth->execute($emailData);
 }
 
 
 function existsEmailInUsers(PDO $dbh, $email)
 {
-	$sql = '
+    $sql = '
 		SELECT COUNT(id)
 		FROM users
 		WHERE email = :email';
-	$sth = $dbh->prepare($sql);
-	$sth->query(['email' => $email]);
-	return ($sth->fetch() > 0);
+    $sth = $dbh->prepare($sql);
+    $sth->query(['email' => $email]);
+
+    return ($sth->fetch() > 0);
 }
 
-function userExists(PDO $dbh, $userId) {
-	$sql = '
+
+function userExists(PDO $dbh, $userId)
+{
+    $sql = '
 		SELECT COUNT(id)
 		FROM users
 		WHERE id = :id';
-	$sth = $dbh->prepare($sql);
-	$sth->query(['id' => $userId]);
-	return ($sth->fetch() > 0);
+    $sth = $dbh->prepare($sql);
+    $sth->query(['id' => $userId]);
+
+    return ($sth->fetch() > 0);
 }
 
 
 function getUserInformation(PDO $dbh, $userId)
 {
-	$sql = '
+    $sql = '
 		SELECT email, name, surname
 		FROM users
 		WHERE id = :id';
-	$sth = $dbh->prepare($sql);
-	$sth->query(['id' => $userId]);
-	return $sth->fetch();
+    $sth = $dbh->prepare($sql);
+    $sth->query(['id' => $userId]);
+
+    return $sth->fetch();
 }
--- src/index.php
+++ src/index.php
@@ -4,52 +4,67 @@ require 'vendor/autoload.php';
 require 'Model.php';
 
 $result = [
-	'surname' => 'John',
-	'lastname' => 'Doe',
-	'email' => 'john@doe.com'
+    'surname' => 'John',
+    'lastname' => 'Doe',
+    'email' => 'john@doe.com'
 ];
 
 $app = new \Slim\Slim();
+
 $app->get('/me', function () use ($app, $result) {
-	$response = $app->response();
-	$response['Content-Type'] = 'application/json';
-	$response->status(200);
-	$response->body(json_encode($result));
+    $response = $app->response();
+    $response['Content-Type'] = 'application/json';
+    $response->status(200);
+    $response->body(json_encode($result));
 });
+
+
 $app->get('/get-info/:id', function ($id) use ($app, $result) {
-	$response = $app->response();
-	$response['Content-Type'] = 'application/json';
-	$pdo = connectDB();
-	if (!userExists($pdo, $id)) {
-		$response->status(400);
-		$response->body(json_encode(['error' => 'User does not exist.']));
-	}
-	$user = getUserInformation($pdo, $id);
-	$response->status(200);
-	$response->body(json_encode([
-		'name' => $user['name'],
-		'surname' => $user['surname'],
-		'email' => $user['email']
+    $response = $app->response();
+    $response['Content-Type'] = 'application/json';
+    $pdo = connectDB();
+
+    if (!userExists($pdo, $id)) {
+        $response->status(400);
+        $response->body(json_encode(['error' => 'User does not exist.']));
+    }
+
+    $user = getUserInformation($pdo, $id);
+
+    $response->status(200);
+    $response->body(json_encode([
+        'name' => $user['name'],
+        'surname' => $user['surname'],
+        'email' => $user['email']
     ]));
 });
-$app->post('/add', function() use ($app) {
-	$response = $app->response();
-	$response['Content-Type'] = 'application/json';
-	$request = $app->request;
-	$email = $request->post('email');
-	$password = $request->post('password');
-	if ($email == null || $password == null) {
-		$response->status(400);
-		$response->body(json_encode(['error' => 'Data are not complete.']));
-	}
-	$pdo = connectDB();
-	if (existsEmailInUsers($pdo, $email)) {
-		$response->status(400);
-		$response->body(json_encode(['error' => 'Email is already registered.']));
-	}
-	registerWithPassword($pdo, $email, $password);
-	$response['Content-Type'] = 'application/json';
-	$response->status(201);
-	$response->body(json_encode(['success' => 'User was created.']));
+
+
+$app->post('/add', function () use ($app) {
+    $response = $app->response();
+    $response['Content-Type'] = 'application/json';
+    $request = $app->request;
+
+    $email = $request->post('email');
+    $password = $request->post('password');
+
+    if ($email == null || $password == null) {
+        $response->status(400);
+        $response->body(json_encode(['error' => 'Data are not complete.']));
+    }
+
+    $pdo = connectDB();
+
+    if (existsEmailInUsers($pdo, $email)) {
+        $response->status(400);
+        $response->body(json_encode(['error' => 'Email is already registered.']));
+    }
+
+    registerWithPassword($pdo, $email, $password);
+
+    $response['Content-Type'] = 'application/json';
+    $response->status(201);
+    $response->body(json_encode(['success' => 'User was created.']));
 });
+
 $app->run();
