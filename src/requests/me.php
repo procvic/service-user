874c22ece9a122f4de7a61087f7083461c72f448
Václav Makeš <info@makes.cz>
2015-08-16 10:26:51 +0200

    Refactor - remove variable from global scope
    The variable was moved to request context.

--- src/requests/me.php
+++ src/requests/me.php
@@ -1,12 +1,12 @@
 <?php
 
-$result = [
-    'surname' => 'John',
-    'lastname' => 'Doe',
-    'email' => 'john@doe.com'
-];
+$app->get('/me', function () use ($app) {
+    $result = [
+        'surname' => 'John',
+        'lastname' => 'Doe',
+        'email' => 'john@doe.com'
+    ];
 
-$app->get('/me', function () use ($app, $result) {
     $response = $app->response();
     $response['Content-Type'] = 'application/json';
     $response->status(200);
